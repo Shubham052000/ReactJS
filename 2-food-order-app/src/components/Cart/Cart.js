c466fd3ffbba8b7b558fa786ac601e492d75632d
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-05-22 19:11:47 +0530

    Submitting & sending the cart and the user data

--- 2-food-order-app/src/components/Cart/Cart.js
+++ 2-food-order-app/src/components/Cart/Cart.js
@@ -37,6 +37,13 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch("https://react-http-174dc-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({ user: userData, orderedItems: cartCtx.items }),
+    });
+  };
+
   const modalActions = (
     <div className={classes.actions}>
       <button className={classes["button--alt"]} onClick={props.onClose}>
@@ -57,7 +64,9 @@ const Cart = (props) => {
         <span>Total amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
