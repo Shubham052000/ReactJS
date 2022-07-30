0c08254c429a179db03e16fdfdaef0b9a40714e5
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-07-30 20:29:27 +0530

    Adding bootstrap to TodoApp

--- 3-to-do-app/src/App.tsx
+++ 3-to-do-app/src/App.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import "./App.css";
+
 import TodoForm from "./components/TodoForm";
 import Todos from "./components/Todos";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export type todoStruct = {
   id: number;
@@ -28,7 +31,10 @@ function App() {
   return (
     <div className="App">
       <h1>The To-do app</h1>
-      <button onClick={(event) => setCreateTodo(!createTodo)}>
+      <button
+        className="btn btn-primary"
+        onClick={(event) => setCreateTodo(!createTodo)}
+      >
         {createTodo ? "Close Todo form" : "Create Todo"}
       </button>
       {createTodo && <TodoForm onAddTodo={(p) => addTodoHandler(p)} />}
