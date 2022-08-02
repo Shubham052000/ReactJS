02451679ecb346bc54cfaf03ab82a19e063b469f
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-08-02 20:55:31 +0530

    Adding a redux store to the todo app

---
+++ 3-to-do-app/src/store/todo-slice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { todoStruct } from "../App";
+
+const initialState = {
+  todos: [
+    { id: 1, title: "React", description: "Learn React basics" },
+    { id: 2, title: "TypeScript", description: "Learn TS basics" },
+  ],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo(state, { payload }) {
+      state.todos = [...state.todos, payload];
+    },
+    removeTodo(state, { payload }) {
+      state.todos = state.todos.filter((todo) => todo.id !== payload);
+    },
+    updateTodo(state, { payload }) {
+      const updateTodoIndex = state.todos.findIndex(
+        (todo) => todo.id === payload.id
+      );
+      state.todos[updateTodoIndex] = payload;
+    },
+  },
+});
+
+export const todoActions = todoSlice.actions;
+
+export default todoSlice;
