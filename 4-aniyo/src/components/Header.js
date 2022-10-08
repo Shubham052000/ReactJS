d5d17504dfb13c01b7ef0e0c0327b3b6cf69f71f
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-10-08 22:10:14 +0530

    Starting with the search functionality

--- 4-aniyo/src/components/Header.js
+++ 4-aniyo/src/components/Header.js
@@ -3,6 +3,7 @@ import { AppBar, Toolbar } from "@mui/material";
 
 import { useNavigate } from "react-router-dom";
 import AniyoLogo from "./logo.png";
+import { Box } from "@mui/system";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -13,12 +14,17 @@ const Header = () => {
   return (
     <AppBar position="absolute">
       <Toolbar sx={{ my: 2 }}>
-        <img
-          src={AniyoLogo}
-          alt="Aniyo Logo"
-          style={{ width: "110px" }}
-          onClick={goBackToHome}
-        />
+        <Box sx={{ flexGrow: 1 }}>
+          <img
+            src={AniyoLogo}
+            alt="Aniyo Logo"
+            style={{ width: "110px" }}
+            onClick={goBackToHome}
+          />
+        </Box>
+        {/* <Box sx={{ width: "12rem" }}>
+          <TextField variant="standard" label="Search" />
+        </Box> */}
       </Toolbar>
     </AppBar>
   );
--- 4-aniyo/src/pages/MainPage.js
+++ 4-aniyo/src/pages/MainPage.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { CssBaseline, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  CssBaseline,
+  TextField,
+  Typography,
+} from "@mui/material";
 import AnimeList from "../components/AnimeList";
+import { Box } from "@mui/system";
 
+const top100Films = [{ title: "Jujutsu kaisen" }, { title: "Tokyo Ghoul" }];
 const MainPage = () => {
   return (
     <>
@@ -18,6 +25,21 @@ const MainPage = () => {
         >
           Discover and share your favorite anime with Aniyo.
         </Typography>
+        <Box display={"flex"} justifyContent="center" mt={5}>
+          <Autocomplete
+            id="free-solo-demo"
+            freeSolo
+            options={top100Films.map((option) => option.title)}
+            renderInput={(params) => (
+              <>
+                <TextField {...params} label="Search" />
+              </>
+            )}
+            sx={{
+              width: "30rem",
+            }}
+          />
+        </Box>
         <AnimeList
           type="Top Anime"
           url={"https://api.jikan.moe/v4/top/anime"}
